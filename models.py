e8bbd01187b175faf2dc9cf7cc15d4d61eb0daee
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-07 20:01:07 -0700

    Remove email field from User model.

--- models.py
+++ models.py
@@ -6,14 +6,12 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
 
     def __repr__(self):
-        return f"User(id='{self.id}', username='{self.username}', email='{self.email}')"
+        return f"User(id='{self.id}', username='{self.username}')"
 
     def json(self):
         return {
             "id": self.id,
-            "username": self.username,
-            "email": self.email
+            "username": self.username
         }
