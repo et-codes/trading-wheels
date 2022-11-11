277a06af0fa563775fdeb94f08de0bf0049b142b
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-11 11:25:28 -0700

    Add porfolio data structure comment.

--- portfolio.py
+++ portfolio.py
@@ -3,5 +3,12 @@ from database import db
 from models import User, Trade
 
 
+# portfolio = [
+#   {symbol, description, shares, price, value, gain},
+#   ...,
+#   {stocks, cash, gain}
+# ]
+
+
 def get_portfolio(username: str) -> list[dict]:
     pass
