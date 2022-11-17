2d3eb7688278ceac54f0eb044cddcb4570f02ed1
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-16 18:11:58 -0700

    Moved functions around.

--- back-end/app/stocks.py
+++ back-end/app/stocks.py
@@ -8,25 +8,6 @@ from flask import request
 
 c = pyEX.Client(version='stable')
 
-def check_for_stale_symbol_list():
-    last_update = Stock.query.first()
-    if last_update is None or \
-        last_update.last_update <= datetime.utcnow() - timedelta(hours=24):
-        print('Refreshing stock symbol table...')
-        refresh_symbols()
-
-def refresh_symbols():
-    delete_stale_symbols()
-    symbol_list = c.symbols(filter='symbol,name')
-    for symbol in symbol_list:
-        stock = Stock(symbol=symbol['symbol'], description=symbol['name'])
-        db.session.add(stock)
-    db.session.commit()
-
-def delete_stale_symbols():
-    Stock.query.delete()
-    db.session.commit()
-
 @app.route('/stock/quote/<string:symbol>')
 def return_stock_data(symbol):
     if not tokens.is_valid(request):
@@ -54,3 +35,22 @@ def get_stock_search_result(fragment):
     check_for_stale_symbol_list()
     stocks = Stock.query.filter(Stock.description.like(f'%{fragment}%')).all()
     return [stock.json() for stock in stocks]
+
+def check_for_stale_symbol_list():
+    last_update = Stock.query.first()
+    if last_update is None or \
+        last_update.last_update <= datetime.utcnow() - timedelta(hours=24):
+        print('Refreshing stock symbol table...')
+        refresh_symbols()
+
+def refresh_symbols():
+    delete_stale_symbols()
+    symbol_list = c.symbols(filter='symbol,name')
+    for symbol in symbol_list:
+        stock = Stock(symbol=symbol['symbol'], description=symbol['name'])
+        db.session.add(stock)
+    db.session.commit()
+
+def delete_stale_symbols():
+    Stock.query.delete()
+    db.session.commit()
