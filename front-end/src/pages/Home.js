bb8cc6be0bacc06577dd579244c57fcc8b577e20
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-17 20:25:56 -0700

    Styling on Home page.

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -14,35 +14,41 @@ const Home = () => {
 
   return (
     <div>
-      <p>
-        Welcome to{' '}
-        <span className="text-primary font-weight-bold">
-          Trading Wheels
-        </span>,
-        a fantasy stock-trading simulation where you can practice your
-        investing skills!
-      </p>
-      <p>
-        To get started, register a new account on our{' '}
-        <BoldLink to="/login">Login</BoldLink> page.
-        You'll start with{' '}
-        <span className="text-success fw-bold">$100,000</span>{' '}
-        virutal cash to start buying stocks. Once you're logged in,
-        you'll have acccess to the{' '}
-        <BoldLink to="/trade">Trade</BoldLink> and{' '}
-        <BoldLink to="/portfolio">Portfolio</BoldLink> pages.
-      </p>
-      <Image src={photo} fluid rounded className="shadow" />
-      <p className="text-muted">
-        Photo by{' '}
-        <a href={links.tyler} target="_blank" rel="noopener noreferrer">
-          Tyler Prahm
-        </a> on{' '}
-        <a href={links.unsplash} target="_blank" rel="noopener noreferrer">
-          Unsplash
-        </a>
-      </p>
-      <div className="d-grid gap-2">
+      <div className="d-flex">
+        <div className="col-4">
+          <p>
+            Welcome to{' '}
+            <span className="text-primary font-weight-bold">
+              Trading Wheels
+            </span>,
+            a fantasy stock-trading simulation where you can practice your
+            investing skills!
+          </p>
+          <p>
+            To get started, register a new account on our{' '}
+            <BoldLink to="/login">Login</BoldLink> page.
+            You'll start with{' '}
+            <span className="text-success fw-bold">$100,000</span>{' '}
+            virutal cash to start buying stocks. Once you're logged in,
+            you'll have acccess to the{' '}
+            <BoldLink to="/trade">Trade</BoldLink> and{' '}
+            <BoldLink to="/portfolio">Portfolio</BoldLink> pages.
+          </p>
+        </div>
+        <div className="col-8">
+          <Image src={photo} fluid rounded className="shadow" />
+          <p className="text-muted text-center">
+            Photo by{' '}
+            <a href={links.tyler} target="_blank" rel="noopener noreferrer">
+              Tyler Prahm
+            </a> on{' '}
+            <a href={links.unsplash} target="_blank" rel="noopener noreferrer">
+              Unsplash
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="d-grid gap-2 mt-3">
         <Button variant="primary" size="lg" as={Link} to="/login">
           Log in to get started!
         </Button>
