59b0f8b550bf293a29555b58c873f4e3f15a17d1
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-17 19:17:19 -0700

    Use NavLink in header.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,25 +1,20 @@
 import { Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
-import { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Header = () => {
-  const [activeKey, setActiveKey] = useState('');
-  const handleSelect = (eventKey) => setActiveKey(eventKey);
-  // How to identify the active page when refreshing screen?
-
   return (
     <Navbar bg="primary" variant="dark" expand="md">
       <Container>
         <NavbarBrand href="/">Trading Wheels</NavbarBrand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
-          <Nav className="me-auto" onSelect={handleSelect} activeKey={activeKey}>
-            <Nav.Link as={NavLink} to="/" eventKey="1">Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/trading" eventKey="2">Trading</Nav.Link>
-            <Nav.Link as={NavLink} to="/portfolio" eventKey="3">Portfolio</Nav.Link>
-            <Nav.Link as={NavLink} to="/about" eventKey="4">About</Nav.Link>
-            <Nav.Link as={NavLink} to="/login" eventKey="5">Login</Nav.Link>
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/trading">Trading</Nav.Link>
+            <Nav.Link as={NavLink} to="/portfolio">Portfolio</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+            <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
