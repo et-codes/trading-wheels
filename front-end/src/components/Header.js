3fdce9a3aa2f87024c357bf52f808386d5430c07
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-17 19:15:31 -0700

    Various tweaks.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,11 +1,12 @@
 import { Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 
 const Header = () => {
   const [activeKey, setActiveKey] = useState('');
   const handleSelect = (eventKey) => setActiveKey(eventKey);
+  // How to identify the active page when refreshing screen?
 
   return (
     <Navbar bg="primary" variant="dark" expand="md">
@@ -14,11 +15,11 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse>
           <Nav className="me-auto" onSelect={handleSelect} activeKey={activeKey}>
-            <Nav.Link as={Link} to="/" eventKey="1">Home</Nav.Link>
-            <Nav.Link as={Link} to="/trading" eventKey="2">Trading</Nav.Link>
-            <Nav.Link as={Link} to="/portfolio" eventKey="3">Portfolio</Nav.Link>
-            <Nav.Link as={Link} to="/about" eventKey="4">About</Nav.Link>
-            <Nav.Link as={Link} to="/login" eventKey="5">Login</Nav.Link>
+            <Nav.Link as={NavLink} to="/" eventKey="1">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/trading" eventKey="2">Trading</Nav.Link>
+            <Nav.Link as={NavLink} to="/portfolio" eventKey="3">Portfolio</Nav.Link>
+            <Nav.Link as={NavLink} to="/about" eventKey="4">About</Nav.Link>
+            <Nav.Link as={NavLink} to="/login" eventKey="5">Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,10 +1,18 @@
+import { Image } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import photo from '../assets/tyler-prahm-lmV3gJSAgbo-unsplash.jpg';
 
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-primary">Trading Wheels</h1>
-      <p>A fantasy stock-trading app.  See what you can do with $100k of virual cash!</p>
+      <p>Welcome to <span className="text-primary font-weight-bold">Trading Wheels</span>, a fantasy stock-trading simulation where you can practice your investing skills!</p>
+      <p>To get started, register a new account on our <Link to="/login"><span className="text-primary font-weight-bold">Login</span></Link> page. You'll start with $100,000 virutal cash to start buying stocks. Once you're logged in, you'll have acccess to the Trade and Portfolio pages.</p>
+      <Image src={photo} fluid rounded className="shadow" />
+      <p className="text-muted">
+        Photo by <a href="https://unsplash.com/@tprahm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tyler Prahm</a> on <a href="https://unsplash.com/s/photos/investing?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      </p>
+
     </div>
   );
 }
