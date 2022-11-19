68163608857cd1ecc088b33bf08633860021d9b0
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-19 12:32:57 -0700

    Prevent navigate to Trading, Portfolio w/o login.

--- front-end/src/pages/Portfolio.js
+++ front-end/src/pages/Portfolio.js
@@ -1,6 +1,10 @@
+import { Navigate } from 'react-router-dom';
 
 
 const Portfolio = ({ username }) => {
+
+  if (!username) return <Navigate to="/" />;
+
   return (
     <div>Portfolio Page</div>
   );
--- front-end/src/pages/Trading.js
+++ front-end/src/pages/Trading.js
@@ -1,9 +1,13 @@
+import { Navigate } from 'react-router-dom';
 
 
 const Trading = ({ username }) => {
+
+  if (!username) return <Navigate to="/" />;
+
   return (
     <div>Trading Page</div>
-  )
+  );
 }
 
-export default Trading
+export default Trading;
