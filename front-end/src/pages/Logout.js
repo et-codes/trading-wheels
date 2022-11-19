523f0a54764e7f6d2c6af9bda4479514fdb8b05c
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-19 12:20:40 -0700

    Change sequence of logout actions.

--- front-end/src/pages/Logout.js
+++ front-end/src/pages/Logout.js
@@ -7,10 +7,10 @@ const Logout = ({ user, setUser, setMessage }) => {
   const logout = async () => {
     try {
       const response = await axios.get(`user/logout/${user}`);
-      setMessage({ text: `Logged out ${response.data}.`, variant: 'secondary' });
-      setUser('');
       localStorage.removeItem('username');
       localStorage.removeItem('token');
+      setUser('');
+      setMessage({ text: `Logged out ${response.data}.`, variant: 'secondary' });
     } catch (error) {
       setMessage({ text: 'Error logging out.', variant: 'danger' });
       console.log(error.response.data);
