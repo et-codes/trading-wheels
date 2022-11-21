c1bd2023b7225fce60aec5951955d837afd2f65a
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-21 14:31:00 -0700

    Clean up Summary.

--- front-end/src/components/Summary.js
+++ front-end/src/components/Summary.js
@@ -1,30 +1,30 @@
-import React from 'react';
 import { currency, percent } from '../utils/format';
 import { Table } from 'react-bootstrap';
 
+
 const Summary = ({ summary }) => {
-  const gainColor = summary.gain_pct >= 0 ? 'text-success' : 'text-danger'
+
+  const gainColor = summary.gain_pct >= 0 ? 'text-success' : 'text-danger';
+
   return (
-    <div>
-      <Table bordered responsive="sm" className="text-center">
-        <thead>
-          <tr className="table-primary">
-            <th>Stock Value</th>
-            <th>Cash Balance</th>
-            <th>Total Assets</th>
-            <th>Gain/Loss</th>
-          </tr>
-        </thead>
-        <tbody className="h5">
-          <tr>
-            <td>{currency(summary.stocks)}</td>
-            <td>{currency(summary.cash)}</td>
-            <td>{currency(summary.total)}</td>
-            <td className={`${gainColor}`}>{percent(summary.gain_pct)}</td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
+    <Table bordered responsive="sm" className="text-center">
+      <thead>
+        <tr className="table-primary">
+          <th>Stock Value</th>
+          <th>Cash Balance</th>
+          <th>Total Assets</th>
+          <th>Gain/Loss</th>
+        </tr>
+      </thead>
+      <tbody className="h5">
+        <tr>
+          <td>{currency(summary.stocks)}</td>
+          <td>{currency(summary.cash)}</td>
+          <td>{currency(summary.total)}</td>
+          <td className={`${gainColor}`}>{percent(summary.gain_pct)}</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
 
