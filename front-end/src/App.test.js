c31848f571c9d18d6541dc79546b2f19142e66a4
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-20 22:41:57 -0700

    Add test for Login page text.

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Login from './pages/Login';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders Trading Wheels on the screen', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Trading Wheels/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders "Trading Wheels" on the home screen', () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const titleText = screen.getAllByText(/Trading Wheels/i);
+  expect(titleText[0]).toBeInTheDocument();
+});
+
+test('renders "Please log in below" on login screen.', () => {
+  render(
+    <Router>
+      <Login />
+    </Router>
+  );
+  const loginText = screen.getByText(/Please log in below/i);
+  expect(loginText).toBeInTheDocument();
 });
