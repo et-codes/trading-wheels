15d69f2870255c48c75f595d81e17ebb9ff4990c
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-21 19:41:40 -0700

    Update StockInfo card.

--- front-end/src/components/StockInfo.js
+++ front-end/src/components/StockInfo.js
@@ -1,14 +1,36 @@
 import axios from 'axios';
-import { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Button, Modal, Table } from 'react-bootstrap';
+import { currency } from '../utils/format';
 
 
 const StockInfo = ({ symbol }) => {
 
   const [show, setShow] = useState(false);
+  const [company, setCompany] = useState({});
+  const [qoute, setQuote] = useState({});
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const MAX_LENGTH = 1024;
+
+  useEffect(() => {
+    if (symbol !== '$CASH') {
+      getStockQuote(symbol);
+      getCompanyInfo(symbol);
+    }
+  }, [symbol]);
+
+  const getStockQuote = async (symbol) => {
+    const response = await axios.get(`/stock/quote/${symbol}`);
+    setQuote(response.data);
+  }
+
+  const getCompanyInfo = async (symbol) => {
+    const response = await axios.get(`/stock/company/${symbol}`);
+    setCompany(response.data);
+  }
 
   return (
     <>
@@ -25,10 +47,34 @@ const StockInfo = ({ symbol }) => {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{symbol}</Modal.Title>
+          <Modal.Title>{company.companyName} ({symbol})</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          Here is some info about {symbol}.
+          <Table size="sm">
+            <tbody>
+              <tr>
+                <td className="text-muted">Latest price/share:</td>
+                <td>{currency(qoute.latestPrice)}</td>
+              </tr>
+              <tr>
+                <td className="text-muted">CEO:</td>
+                <td>{company.CEO}</td>
+              </tr>
+              <tr>
+                <td className="text-muted">Exchange:</td>
+                <td>{qoute.primaryExchange}</td>
+              </tr>
+              <tr>
+                <td className="text-muted">Location:</td>
+                <td>{company.city}, {company.state}, {company.country}</td>
+              </tr>
+            </tbody>
+          </Table>
+          <p className="text-muted">
+            {String(company.description).length > MAX_LENGTH
+              ? company.description.slice(0, MAX_LENGTH) + '...'
+              : company.description}
+          </p>
         </Modal.Body>
       </Modal>
     </>
