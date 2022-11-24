f76fa41cf24aa6d6c76d7b6fda9934d0bb44dcb5
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-23 23:44:52 -0700

    Add search input controls.

--- front-end/src/pages/Trading.js
+++ front-end/src/pages/Trading.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import httpClient from "../utils/httpClient";
 
 
 const Trading = ({ username, setMessage }) => {
+
+  const [search, setSearch] = useState('');
+  const [searchType, setSearchType] = useState('Symbol');
 
   if (!username) {
     setMessage({
@@ -11,8 +17,52 @@ const Trading = ({ username, setMessage }) => {
     return <Navigate to="/login" />;
   }
 
+  const handleSelect = (event) => {
+    setSearchType(event.target.value);
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Submit.');
+  }
+
   return (
-    <div>Trading Page</div>
+    <div>
+      <h2>Trading</h2>
+      <em className='text-muted'>
+        Search for stocks by ticker symbol or company name.
+      </em>
+
+      <Form onSubmit={handleSubmit}>
+        <Row>
+          <Col xs lg="3">
+            <Form.Select onChange={handleSelect} defaultValue="Symbol">
+              <option>Symbol</option>
+              <option>Company name</option>
+            </Form.Select>
+          </Col>
+          <Col>
+            <Form.Control
+              type="text"
+              placeholder="Search for..."
+              data-testid="search-input"
+              value={search}
+              onChange={handleSearch}
+            />
+          </Col>
+          <Col md="auto">
+            <Button variant="primary" type="submit" data-testid="search-button">
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+
+    </div>
   );
 }
 
