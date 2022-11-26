71bd6d7212efd29d48cc33e52382cbf088f856ce
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-25 17:31:28 -0700

    Early return CASH from /stock/<symbol> request.

--- back-end/app/stocks.py
+++ back-end/app/stocks.py
@@ -23,6 +23,7 @@ def return_stock_search_result(fragment):
 
 @app.route('/stock/<string:symbol>')
 def return_stock_data(symbol):
+    if symbol == '$CASH': return CASH
     if not user_is_authorized():
         return 'Not authorized.', 401
     return get_stock_data(symbol)
@@ -39,8 +40,6 @@ def get_stock_data(symbol):
     }
 
 def get_stock_quote(symbol):
-    if symbol == '$CASH':
-        return CASH
     try:
         quote = c.quote(symbol, filter='symbol,companyName,latestPrice,primaryExchange')
         return quote
