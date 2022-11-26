0843575b8e242ee365869e8e88070594073e6b34
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-26 09:18:41 -0700

    Disable strict mode.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,9 +7,9 @@ import './styles/sandstone_theme.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
