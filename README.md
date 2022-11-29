9c76c79f09ccfe252cbdf26a1c00171aea1f27a9
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-29 00:01:53 -0700

    Create About page content.

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ Trading Wheels is a stock trading simulator, currently under development.
 
 ## TODO list
 
-- Return search results by pages - also, what is "too many" results? Set a limit?
+- Generate About page.
 - Add type hints to the Python files functions.
 - What happens in the front end when the server session is expired?
 - Check for error handling where needed.
 - Write tests for the front end.
-- Check if backend tests are working after changing to session authorization.
-- Fix the damned bugs in the database hits for sessions.
 
 ## Notes
 
@@ -22,23 +20,3 @@ Trading Wheels is a stock trading simulator, currently under development.
   ...,
   {stocks, cash, total, gain_pct}
   ]
-
-## IEX API notes
-
-- Using Python module PyEX: https://iexcloud.io/blog/how-to-get-market-data-in-python
-  https://pypi.org/project/pyEX/
-
-- get list of symbols and company names:
-  https://iexcloud.io/documentation/using-core-data/finding-symbols.html
-  https://cloud.iexapis.com/stable/ref-data/symbols?filter=symbol,name&token={...}
-- historical data for charting:
-  https://iexcloud.io/docs/api/#historical-prices
-  Example: https://cloud.iexapis.com/stable/stock/{symbol}/chart/3m?token={...}
-- get company logo URL:
-  /stock/{symbol}/logo
-- get price only: /stock/{symbol}/price
-- search stock symbols: /search/{fragment}
-
-## Server-side sessions
-
-https://www.youtube.com/watch?v=sBw0O5YTT4Q&ab_channel=DevGuyAhnaf
--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -1,8 +1,47 @@
+import ReactMarkdown from 'react-markdown';
+import projectPlan from './images/project-plan.svg';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 
 const About = () => {
+
+  const heading = `# About Trading Wheels
+
+Trading Wheels was developed by Eric Thornton in November 2022 
+as a means to demonstrate full-stack web application technologies. Below are some notable details, along with my original sketch for the project.
+
+Source code is available on my [GitHub]("https://github.com/et-codes/trading-wheels") page.`;
+
+  const body = `## Server details
+
+* The backend code is written in Python.
+* The client bundle and API endpoints are served with the Flask framework.
+* The interface with the PostgreSQL database is implemented with \`Flask-SQLAlchemy\`.
+* \`Flask-Migrations\` is used for building and updating the database tables based on the declarative models.
+* Login persistence is managed with server sessions.
+
+## Client details
+
+* The frontend code is written in JavaScript using the React framework.
+* HTTP requests to the API are done using the \`axios\` library.
+* \`Bootstrap\` and \`React-Bootstrap\` are used for styling.
+* \`react-google-charts\` is used to create the stock charts.
+`;
+
   return (
-    <div>About page</div>
+    <Container>
+      <Row>
+        <ReactMarkdown children={heading} />
+      </Row>
+      <Row>
+        <Col md={6}>
+          <ReactMarkdown children={body} />
+        </Col>
+        <Col md={6}>
+          <Image src={projectPlan} fluid rounded className="shadow" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
