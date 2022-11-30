31362908fa3cad6fa8efa7dddd2a7d280cebd5b9
et-codes <104942211+et-codes@users.noreply.github.com>
2022-11-29 17:34:12 -0700

    Update remember-me cookie settings.

--- back-end/config.py
+++ back-end/config.py
@@ -4,11 +4,9 @@ from datetime import timedelta
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY= os.environ.get('SECRET_KEY') or 'GetYourOwnSecretKey'
-
+    SECRET_KEY= os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SESSION_PERMANENT = True
-    PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+    REMEMBER_COOKIE_DURATION  = timedelta(days=7)
+    REMEMBER_COOKIE_REFRESH_EACH_REQUEST = True
