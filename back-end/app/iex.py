9760484675c08cf93e6217269ebb14cb223ab97d
et-codes <104942211+et-codes@users.noreply.github.com>
2022-12-15 20:15:14 -0700

    Update formatting.

--- back-end/app/iex.py
+++ back-end/app/iex.py
@@ -1,27 +1,25 @@
 import pyEX
 
-api = pyEX.Client(version='stable')
+api = pyEX.Client(version="stable")
+
 
 def quote(symbol: str):
-    quote_filter = 'symbol,companyName,latestPrice,primaryExchange'
-    company_filter = 'CEO,city,state,country,description'
-    chart_filter = 'date,volume,close'
+    quote_filter = "symbol,companyName,latestPrice,primaryExchange"
+    company_filter = "CEO,city,state,country,description"
+    chart_filter = "date,volume,close"
 
     try:
         quote = api.quote(symbol, filter=quote_filter)
         company_data = api.company(symbol, filter=company_filter)
-        chart = api.chart(symbol, timeframe='3m', sort="asc", filter=chart_filter)
+        chart = api.chart(symbol, timeframe="3m", sort="asc", filter=chart_filter)
     except pyEX.common.exception.PyEXception:
         return f'Symbol "{symbol}" not found.', 404
 
-    return {
-        'quote': quote,
-        'company': company_data,
-        'chart': chart
-    }
+    return {"quote": quote, "company": company_data, "chart": chart}
+
 
 def get_symbols():
     try:
-        return api.symbols(filter='symbol,name')
+        return api.symbols(filter="symbol,name")
     except pyEX.common.exception.PyEXception:
-        return f'Error retreiving symbols.', 500
+        return f"Error retreiving symbols.", 500
